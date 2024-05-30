ad34a344cc24987be38f8c8f458b870dec028b8a
Shaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-05-29 17:16:14 -0700

    refactor: brain mush

--- home/shaun/common/core/zsh/p10k/p10k.zsh
+++ home/shaun/common/core/zsh/p10k/p10k.zsh
@@ -32,7 +32,7 @@
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
-    # host
+    host
     dir                     # current directory
     vcs                     # git status
     # =========================[ Line #2 ]=========================
