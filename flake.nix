acf9451b6084036ebfb2da57454b1ebf3e09faa5
Shaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-05-31 01:26:01 -0700

    feat: use catppuccin flake for theming

--- flake.nix
+++ flake.nix
@@ -40,6 +40,8 @@
       url = "github:oxalica/nil";
     };
 
+    catppuccin.url = "github:catppuccin/nix";
+
     # vscode extensions/themes
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
     catppuccin-vsc.url = "github:catppuccin/vscode";
--- home/shaun/common/optional/desktops/gtk.nix
+++ home/shaun/common/optional/desktops/gtk.nix
@@ -4,31 +4,30 @@
   gtk = {
     enable = true;
     #font.name =  TODO see misterio https://github.com/Misterio77/nix-config/blob/f4368087b0fd0bf4a41bdbf8c0d7292309436bb0/home/misterio/features/desktop/common/gtk.nix   he has a custom config for managing fonts, colorsheme etc.
-
-    theme = {
-      name = "Catppuccin-Macchiato-Standard-Blue-Dark";
-      package = pkgs.catppuccin-gtk.override {
-        accents = [ "blue" ];
-        size = "standard";
-        tweaks = [ "normal" ];
-        variant = "macchiato";
-      };
+    catppuccin = {
+      enable = true;
+      cursor.enable = true;
     };
-
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
-    };
-
-    #TODO add ascendancy cursor pack
-    #cursortTheme.name = "";
-    #cursortTheme.package = ;
-
-    #gtk3.extraConfig = {
-    #  Settings = ''
-    #    gtk-application-prefer-dark-theme=1
-    #  '';
+    #theme = {
+    #  name = "Catppuccin-Macchiato-Standard-Blue-Dark";
+    #  package = pkgs.catppuccin-gtk.override {
+    #    accents = [ "blue" ];
+    #    size = "standard";
+    #    tweaks = [ "normal" ];
+    #    variant = "macchiato";
+    #  };
     #};
+
+    #iconTheme = {
+    #  name = "Papirus-Dark";
+    #  package = pkgs.papirus-icon-theme;
+    #};
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
     gtk4.extraConfig = {
       Settings = ''
         gtk-application-prefer-dark-theme=1
