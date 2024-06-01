5b869ef4b881c440cbd645e8f50f379590dcb4da
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-06-01 01:27:40 +0000

    feat: add host corais

---
+++ home/shaun/corais.nix
@@ -0,0 +1,56 @@
+{ lib, configVars, ... }:
+{
+  imports = [
+    #################### Required Configs ####################
+    common/core #required
+
+    #################### Host-specific Optional Configs ####################
+    common/optional/sops.nix
+    common/optional/browsers/brave.nix
+    common/optional/browsers/firefox.nix
+    common/optional/comms/discord.nix
+    common/optional/dev/vscode.nix
+    common/optional/games/steam.nix
+    common/optional/games/heroic.nix
+    common/optional/media/calibre.nix
+    common/optional/tools/eye-of-gnome.nix
+    common/optional/tools/gimp.nix
+    common/optional/tools/inkscape.nix
+    common/optional/tools/libreoffice.nix
+    common/optional/tools/wine.nix
+    common/optional/tools/xdg.nix
+    common/optional/tools/nemo.nix
+
+    common/optional/desktops
+
+  ];
+
+  monitors = [
+    {
+      name = "DP-1";
+      width = 2560;
+      height = 1440;
+      refreshRate = 165;
+      noBar = false;
+      x = 0;
+      workspace = "1";
+      primary = true;
+    }
+    {
+      name = "DP-2";
+      width = 2560;
+      height = 1440;
+      refreshRate = 165;
+      x = 2560;
+      workspace = "F1";
+    }
+  ];
+
+  home = {
+    username = configVars.username;
+    homeDirectory = "/home/${configVars.username}";
+  };
+  # Disable impermanence
+  #home.persistence = lib.mkForce { };
+
+}
