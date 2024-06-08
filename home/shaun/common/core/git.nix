63acd90b4fffb2e7da2db3b3c495b7319601498d
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-06-07 22:02:56 -0700

    actually use github no-reply email

--- home/shaun/common/core/git.nix
+++ home/shaun/common/core/git.nix
@@ -5,8 +5,7 @@
     enable = true;
     package = pkgs.gitAndTools.gitFull;
     userName = configVars.handle;
-    # Not best practice, but this is primarily being hidden from the open internet - for more information see https://github.com/ryantm/agenix?tab=readme-ov-file#builtinsreadfile-anti-pattern
-    userEmail = configLib.readFile config.sops.secrets."${configVars.username}/email".path;
+    userEmail = configVars.gitEmail;
     aliases = {
       stat = "status";
     };
