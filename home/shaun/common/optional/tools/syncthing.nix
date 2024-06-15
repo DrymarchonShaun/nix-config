be7cbe9da830ccbf5341df257e265314b8a39033
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-06-14 19:50:02 -0700

    various changes

--- home/shaun/common/optional/tools/syncthing.nix
+++ home/shaun/common/optional/tools/syncthing.nix
@@ -1,4 +1,7 @@
-{ configLib, config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+let
+  cfg = config.services.syncthing.tray;
+in
 {
   home.packages = [ pkgs.syncthing-resolve-conflicts ];
   services.syncthing = {
@@ -9,16 +12,25 @@
       package = pkgs.syncthingtray;
     };
   };
-  # config = configLib.mkDelayedUnit pkgs config.services.syncthing.tray.package "/bin/syncthingtry --wait";
-  systemd.user.services.syncthingtray = {
+
+  systemd.user.services.${cfg.package.pname} = {
     Unit = {
       StartLimitBurst = 30;
     };
 
     Service = {
       RestartSec = 5;
-      ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
-      ExecStart = "${config.services.syncthing.tray.package}/bin/syncthingtray --wait";
+      # Make it look like ${cfg.package} starts immediately, but actually delay it for a few seconds.
+      # We need to make systemd think it already started, so that sway can finish starting
+      # `graphical-session.target`, after which it starts `xdg-desktop-portal.service`. Once the
+      # desktop portal is started, waybar can finally finish initializing the tray, and at that
+      # point syncthingtray can start without crashing.
+      ExecStart = lib.mkForce (pkgs.writeShellScript "${cfg.package.pname}-delayed" ''
+        set -euo pipefail
+        ${lib.getExe' pkgs.coreutils "sleep"} 3
+        exec ${lib.getExe' cfg.package cfg.command}
+      '');
+
       Restart = "on-failure";
       KillMode = "mixed";
     };
