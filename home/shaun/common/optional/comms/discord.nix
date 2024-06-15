7126f25343a0f205c397d654aba7aa097f1b2c35
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-06-14 19:50:02 -0700

    various changes

--- home/shaun/common/optional/comms/discord.nix
+++ home/shaun/common/optional/comms/discord.nix
@@ -1,43 +1,6 @@
-{ pkgs, ... }:
-let
-  vencord = pkgs.discord.override { withVencord = true; };
-  krisp-patcher = pkgs.writers.writePython3Bin "krisp-patcher"
-    {
-      libraries = with pkgs.python3Packages; [ capstone pyelftools ];
-      flakeIgnore = [
-        "E501" # line too long (82 > 79 characters)
-        "F403" # ‘from module import *’ used; unable to detect undefined names
-        "F405" # name may be undefined, or defined from star imports: module
-      ];
-    }
-    (builtins.readFile (builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/sersorrel/sys/main/hm/discord/krisp-patcher.py";
-      sha256 = "sha256-87VlZKw6QoXgQwEgxT3XeFY8gGoTDWIopGLOEdXkkjE=";
-    }));
-
-  discord-patched-launch = pkgs.writeScriptBin "discord" ''
-    while true; do
-      if [ ! -f ~/.config/discord/${vencord.version}/modules/discord_krisp/discord_krisp.node ]; then
-        ${vencord}/bin/discord &
-        notify-send "Failed to Apply Patch - Relaunching Discord" "File /modules/discord_krisp/discord_krisp.node does not exist"
-        
-        # Wait for the file to appear
-        while [ ! -f ~/.config/discord/${vencord.version}/modules/discord_krisp/discord_krisp.node ]; do
-          sleep 1
-        done
-        
-        # Kill Discord process
-        killall .Discord-wrapped
-      else
-        ${krisp-patcher}/bin/krisp-patcher "$(realpath ~/.config/discord/${vencord.version}/modules/discord_krisp/discord_krisp.node)"
-        ${vencord}/bin/discord
-          break
-      fi
-    done
-  '';
-in
+{ lib, pkgs, ... }:
 {
-  home.packages = [ discord-patched-launch ];
+  home.packages = [ pkgs.discord-patched-launcher ];
   systemd.user.services.discord = {
     Unit = {
       StartLimitBurst = 30;
@@ -51,8 +14,13 @@ in
     Service = {
       RestartSec = 5;
       Restart = "on-failure";
-      ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
-      ExecStart = "${discord-patched-launch}/bin/discord";
+      # ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
+      # ExecStart = "${pkgs.discord-patched-launcher}/bin/discord";
+      ExecStart = lib.mkForce (pkgs.writeShellScript "discord-delayed" ''
+        set -euo pipefail
+        ${lib.getExe' pkgs.coreutils "sleep"} 3
+        exec ${lib.getExe' pkgs.discord-patched-launcher "discord" }
+      '');
       KillMode = "mixed";
     };
 
