2299f6fc80815c8b8fac27b56a4a0b65c9cc6fa6
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-06-20 19:51:07 -0700

    various changes

---
+++ hosts/common/core/fonts.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }: {
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      roboto
+      roboto-mono
+      liberation_ttf
+      (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
+    ];
+
+    fontconfig = {
+      defaultFonts = {
+        serif = [ "Liberation Serif" "Symbols Nerd Font Mono" ];
+        sansSerif = [ "Roboto" "Symbols Nerd Font Mono" ];
+        monospace = [ "Roboto Mono" "Symbols Nerd Font Mono" ];
+      };
+    };
+  };
+}
