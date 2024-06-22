9b4c70234677a289399dafed9a7735654d81584b
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-06-21 20:11:59 -0700

    broken - still testing

--- pkgs/arma3-unix-launcher/default.nix
+++ pkgs/arma3-unix-launcher/default.nix
@@ -1,17 +1,17 @@
 { clangStdenv
 , lib
+, curl
+, substituteAll
 , fetchFromGitHub
-, wrapQtAppsHook
-, git
+, fetchurl
 , cmake
-, pkg-config
 , curlpp
+, doctest
 , spdlog
 , fmt
-, pugixml
+, trompeloeil
 , nlohmann_json
-, qtbase
-, qtsvg
+, qt5
 }:
 clangStdenv.mkDerivation {
   pname = "arma3-unix-launcher";
@@ -22,18 +22,40 @@ clangStdenv.mkDerivation {
     rev = "commit-383";
     hash = "sha256-1CXWwujLgNfofTmKkFqaCUGwQTE7QIfhulOwHfhsTy0=";
   };
-  nativeBuildInputs = [ wrapQtAppsHook cmake pkg-config ];
+  nativeBuildInputs = [ cmake curl.dev ];
 
   buildInputs = [
-    curlpp
-    (spdlog.overrideAttrs { version = "1.11.0"; })
-    fmt
-    nlohmann_json
-    pugixml
-    qtbase
-    qtsvg
+    qt5.qtbase
+    qt5.qtsvg
   ];
 
+  cmakeFlags = [ "-Wno-dev" ];
+  patches = [
+    # prevent CMake from trying to get libraries on the Internet
+    (substituteAll {
+      src = ./dont_fetch_dependencies.patch;
+      argparse_src = fetchFromGitHub {
+        owner = "p-ranav";
+        repo = "argparse";
+        rev = "45664c4";
+        sha256 = "sha256-qLD9zD6hbItDn6ZHHWBXrAWhySvqcs40xA5+C/5Fkhw=";
+      };
+      curlpp_src = curlpp.src.overrideAttrs { version = "master"; rev = "master"; };
+      doctest_src = doctest.src;
+      fmt_src = fmt.src.overrideAttrs { version = "8.1.1"; rev = "8.1.1"; };
+      nlohmann_json_src = nlohmann_json.overrideAttrs { dontBuild = true; version = "3.7.3"; };
+      pugixml_src = fetchFromGitHub {
+        owner = "muttleyxd";
+        repo = "pugixml";
+        rev = "simple-build-for-a3ul";
+        sha256 = "sha256-FpREdz6DbhnLDGOuQY9rU17SSd6ngA4WfO0kGHqGJPM=";
+      };
+      spdlog_src = spdlog.src.overrideAttrs { version = "v1.x"; rev = "v1.x"; };
+      steamworkssdk_src = fetchurl { url = "https://github.com/julianxhokaxhiu/SteamworksSDKCI/releases/download/1.53/SteamworksSDK-v1.53.0_x64.zip"; sha256 = "sha256-6PQGaPsaxBg/MHVWw2ynYW6LaNSrE9Rd9Q9ZLKFGPFA="; };
+      trompeloeil_src = trompeloeil.src.overrideAttrs { version = "64fd171"; rev = "64fd171"; };
+    })
+  ];
+  dontWrapQtApps = true;
   installPhase = ''
     mkdir -p $out/{bin,share}
     mv share $out/
