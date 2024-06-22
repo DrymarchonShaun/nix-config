82a94652506a6ae8023aa4f5a52e772618d40a7b
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-06-22 10:08:12 -0700

    various changes

--- home/shaun/common/optional/tools/wine.nix
+++ home/shaun/common/optional/tools/wine.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    wineWowPackages.stagingFull
+    # for some ungodly reason wineWowPackages.stagingFull currently 
+    # tries to compile gcc so using ubstable version for now
+    unstable.wineWowPackages.stagingFull
     winetricks
   ];
 }
