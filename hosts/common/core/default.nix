0ba767a42f68eee1a9c218b2feab9386ae905a32
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-06-23 00:05:33 -0700

    less delay on failed login

--- hosts/common/core/default.nix
+++ hosts/common/core/default.nix
@@ -4,9 +4,22 @@
     ++ (builtins.attrValues outputs.nixosModules);
 
   #services.yubikey-agent.enable = true;
-  security.pam.services.swaylock = { };
 
-
+  # less delay on failed login
+  security.pam.services.login = {
+    nodelay = true;
+    failDelay = {
+      enable = true;
+      delay = 500000;
+    };
+  };
+  security.pam.services.sudo = {
+    nodelay = true;
+    failDelay = {
+      enable = true;
+      delay = 500000;
+    };
+  };
   security.sudo.extraConfig = ''
     Defaults timestamp_timeout=120 # only ask for password every 2h
     # Keep SSH_AUTH_SOCK so that pam_ssh_agent_auth.so can do its magic.
