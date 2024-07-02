59665aa352f19feb8447e1a16fed141eb7c0958b
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-07-02 12:35:49 -0700

    fix: upstream patch for nemo

--- overlays/default.nix
+++ overlays/default.nix
@@ -28,7 +28,13 @@
       nemo = cprev.nemo.overrideAttrs (attrs: {
         patches = (attrs.patches or [ ]) ++ [
           # https://github.com/NixOS/nixpkgs/issues/212740
-          ./patches/nemo-no-widget-destroy.patch
+          # ./patches/nemo-no-widget-destroy.patch
+          # Don't tie the interactive search box with the window's lifetime
+          # https://github.com/linuxmint/nemo/issues/3423
+          (prev.fetchpatch {
+            url = "https://github.com/linuxmint/nemo/commit/055b47af0e1a830e556989372f3689bbd36b639d.patch";
+            hash = "sha256-cMtBz1uLGCV4nnrBITzXAV/SffI0nNcEdeujrHxAzQc=";
+          })
         ];
       });
     });
