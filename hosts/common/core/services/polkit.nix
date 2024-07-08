5846f7d7a889d107cf9bf46632f7a7be6023d1d7
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-07-08 11:57:50 -0700

    fix: actually import files from hosts/common/core/services/

--- hosts/common/core/services/polkit.nix
+++ hosts/common/core/services/polkit.nix
@@ -10,21 +10,23 @@
     polkit.addRule(function(action, subject) {
         var YES = polkit.Result.YES;
         var permission = {
+        "org.freedesktop.udisks.drive-detach": YES,
+        "org.freedesktop.udisks.drive-eject": YES,
         "org.freedesktop.udisks.filesystem-mount": YES,
         "org.freedesktop.udisks.luks-unlock": YES,
-        "org.freedesktop.udisks.drive-eject": YES,
-        "org.freedesktop.udisks.drive-detach": YES,
-        "org.freedesktop.udisks2.filesystem-mount": YES,
-        "org.freedesktop.udisks2.encrypted-unlock": YES,
         "org.freedesktop.udisks2.eject-media": YES,
-        "org.freedesktop.udisks2.power-off-drive": YES,
-        "org.freedesktop.udisks2.filesystem-mount-other-seat": YES,
-        "org.freedesktop.udisks2.filesystem-unmount-others": YES,
-        "org.freedesktop.udisks2.encrypted-unlock-other-seat": YES,
         "org.freedesktop.udisks2.eject-media-other-seat": YES,
+        "org.freedesktop.udisks2.encrypted-unlock": YES,
+        "org.freedesktop.udisks2.encrypted-unlock-other-seat": YES,
+        "org.freedesktop.udisks2.filesystem-mount": YES,
+        "org.freedesktop.udisks2.filesystem-mount-other-seat": YES,
+        "org.freedesktop.udisks2.filesystem-mount-system": YES,
+        "org.freedesktop.udisks2.filesystem-mount-system-internal": YES,
+        "org.freedesktop.udisks2.filesystem-unmount-others": YES,
+        "org.freedesktop.udisks2.power-off-drive": YES,
         "org.freedesktop.udisks2.power-off-drive-other-seat": YES
         };
-        if (subject.isInGroup("storage")) {
+        if (subject.local && subject.active && subject.isInGroup("storage")) {
         return permission[action.id];
         }
     });
