a2f2056e0b0f51b3ff4e30cf79a146f08a0c8faa
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-07-15 18:04:56 -0700

    feat: git changes

--- home/shaun/common/core/git.nix
+++ home/shaun/common/core/git.nix
@@ -1,11 +1,17 @@
-{ pkgs, lib, config, configVars, configLib, ... }:
+{ pkgs, lib, config, configLib, configVars, ... }:
+let
+  handle = configVars.handle;
+  publicGitEmail = configVars.gitHubEmail;
+  publicKey = "${config.home.homeDirectory}].ssh/id_mimir.pub";
+  username = configVars.username;
+in
 {
   home.packages = [ pkgs.lazygit ];
   programs.git = {
     enable = true;
     package = pkgs.gitAndTools.gitFull;
-    userName = configVars.handle;
-    userEmail = configVars.gitHubEmail;
+    userName = handle;
+    userEmail = publicGitEmail;
     aliases = {
       stat = "status";
     };
@@ -19,8 +25,23 @@
           insteadOf = "https://gitlab.com";
         };
       };
-    };
 
+      #FIXME stage 3 - Re-enable signing. needs additional setup
+      commit.gpgsign = false;
+      gpg.format = "ssh";
+      user.signing.key = "${publicKey}";
+      # Taken from https://github.com/clemak27/homecfg/blob/16b86b04bac539a7c9eaf83e9fef4c813c7dce63/modules/git/ssh_signing.nix#L14
+      gpg.ssh.allowedSignersFile = "${config.home.homeDirectory}/.ssh/allowed_signers";
+    };
+    signing = {
+      signByDefault = true;
+      key = publicKey;
+    };
     ignores = [ ".direnv" "result" ];
   };
-} 
+  # NOTE: To verify github.com update commit signatures, you need to manually import
+  # https://github.com/web-flow.gpg... would be nice to do that here
+  home.file.".ssh/allowed_signers".text = ''
+    ${publicGitEmail} ${lib.fileContents (configLib.relativeToRoot "hosts/common/users/${username}/keys/id_mimir.pub")}
+  '';
+}
