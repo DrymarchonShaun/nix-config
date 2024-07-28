da0ca6dd6355b79633528dae849621d1c7e80538
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-07-28 14:05:44 -0700

    fix: re-add amdgpu.dcdebugmask kernel parameter

--- hosts/corais/hardware-configuration.nix
+++ hosts/corais/hardware-configuration.nix
@@ -11,7 +11,7 @@
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "uas" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
-  boot.kernelParams = [ ];
+  boot.kernelParams = [ "amdgpu.dcdebugmask=0x10" ];
   boot.extraModulePackages = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
