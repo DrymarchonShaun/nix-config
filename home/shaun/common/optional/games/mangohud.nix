160523835b1f00f3d3ca4aa0dc640c4c72606284
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-07-29 00:06:08 -0700

    fix: use mangohud config in gamescope

--- home/shaun/common/optional/games/mangohud.nix
+++ home/shaun/common/optional/games/mangohud.nix
@@ -1,16 +1,23 @@
-{ ... }: {
+{ config, ... }: {
+  home.sessionVariables = {
+    MANGOHUD_CONFIGFILE = config.xdg.configFile."MangoHud/MangoHud.conf".target;
+  };
   programs.mangohud = {
     enable = true;
     enableSessionWide = true;
     settings = {
+      cpu_power = true;
       fps = true;
       gamemode = true;
+      gpu_power = true;
       gpu_stats = true;
       gpu_temp = true;
+      mangoapp_steam = true;
       no_display = true;
       output_folder = "/home/shaun";
       ram = true;
       resolution = true;
+      toggle_hud = "Super_R";
       vram = true;
       vulkan_driver = true;
       wine = true;
