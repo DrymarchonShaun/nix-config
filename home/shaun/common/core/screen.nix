21e3e062ea6c8de99dfc9770d6301cd06887240f
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-07-30 01:39:59 -0700

    fix: move screen config from ~/

--- home/shaun/common/core/screen.nix
+++ home/shaun/common/core/screen.nix
@@ -1,6 +1,9 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   home.packages = [ pkgs.screen ];
-  home.file.".screenrc".text = ''
+  home.sessionVariables = {
+    SCREENRC = "${config.home.homeDirectory}/.config/screen/screenrc";
+  };
+  home.file.".config/screen/screenrc".text = ''
     startup_message off
     defbce on
     setenv TERM xterm-256color
