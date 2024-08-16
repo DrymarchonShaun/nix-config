28d1c91e4417bd98266d409bf3772706dc3f086e
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-08-16 11:47:02 -0700

    fix: don't install wootility

--- hosts/common/optional/wooting.nix
+++ hosts/common/optional/wooting.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }:
 {
   hardware.wooting.enable = true;
-  environment.systemPackages = [ pkgs.wootility ];
+  # use https://beta.wooting.io for the latest firmware instead
+  # environment.systemPackages = [ pkgs.wootility ];
 }
