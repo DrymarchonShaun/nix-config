f08d1e42161b8907c7db84d8d8c8c168b6e893cb
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-08-21 11:10:06 -0700

    chore: use nixpkgs version of arma3-unix-launcher

--- home/shaun/common/optional/games/steam.nix
+++ home/shaun/common/optional/games/steam.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 {
   home.packages = [
-    pkgs.arma3-unix-launcher
-    (pkgs.arma3-unix-launcher.override { buildDayZLauncher = true; })
+    pkgs.unstable.arma3-unix-launcher
+    (pkgs.unstable.arma3-unix-launcher.override { buildDayZLauncher = true; })
     pkgs.gamma-launcher
     pkgs.unstable.gamescope
   ];
