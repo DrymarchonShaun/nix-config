609b0250f19d76371dadb7a943a30c167281c434
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-09-24 16:13:05 -0700

    fix: make thunars "open terminal here" functionality to open foot

--- home/shaun/common/optional/tools/thunar.nix
+++ home/shaun/common/optional/tools/thunar.nix
@@ -4,27 +4,27 @@
   config,
   ...
 }:
+let
+  startInDirectoryScript = pkgs.writeShellScript "exec-terminal-in-directory" ''
+    ${config.programs.foot.package}/bin/foot --working-directory "$@"
+  '';
+in
 {
-  xdg.configFile."Thunar/uca.xml".content = pkgs.substituteAll {
-    src = ''
-        <?xml version="1.0" encoding="UTF-8"?>
-        <actions>
-        <action>
-              <icon>utilities-terminal</icon>
-              <name>Open Terminal Here</name>
-              <submenu></submenu>
-              <unique-id>1685763450018352-1</unique-id>
-              <command>@startInDirectoryScript@ %f</command>
-              <description>Open the preferred terminal in the current directory.</description>
-              <range></range>
-              <patterns>*</patterns>
-              <startup-notify/>
-              <directories/>
-      </action>
-      </actions>
-    '';
-    startInDirectoryScript = pkgs.writeShellScript "exec-terminal-in-directory" ''
-      ${config.programs.foot.package}/bin/foot --working-directory "$@"
-    '';
-  };
+  xdg.configFile."Thunar/uca.xml".text = ''
+      <?xml version="1.0" encoding="UTF-8"?>
+      <actions>
+      <action>
+            <icon>utilities-terminal</icon>
+            <name>Open Terminal Here</name>
+            <submenu></submenu>
+            <unique-id>1685763450018352-1</unique-id>
+            <command>${startInDirectoryScript} %f</command>
+            <description>Open the preferred terminal in the current directory.</description>
+            <range></range>
+            <patterns>*</patterns>
+            <startup-notify/>
+            <directories/>
+    </action>
+    </actions>
+  '';
 }
