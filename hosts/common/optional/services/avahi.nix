98a581c094868225de167a9ea2bedb1998551ec2
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-09-24 16:46:27 -0700

    fix: properly enable avahi

--- hosts/common/optional/services/avahi.nix
+++ hosts/common/optional/services/avahi.nix
@@ -3,6 +3,10 @@
   services.avahi = {
     enable = true;
     nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+    };
     denyInterfaces = [
       "podman0"
       "veth@if2"
