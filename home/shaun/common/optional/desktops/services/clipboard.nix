c706777fb3dd586fef23586edeb0edfe75d2955e
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-10-12 14:51:57 -0700

    fix: readd wl-clip-persist

--- home/shaun/common/optional/desktops/services/clipboard.nix
+++ home/shaun/common/optional/desktops/services/clipboard.nix
@@ -3,14 +3,24 @@
   home.packages = builtins.attrValues {
     inherit (pkgs)
       wl-clipboard
+      wl-clip-persist
       xclip
       ;
   };
-  #services.cliphist = {
-  #  enable = true;
-  #  allowImages = true;
-  #};
+
   services.copyq = {
     enable = true;
+  };
+
+  systemd.user.services.wl-clip-persist = {
+    Unit = {
+      description = "Keep Wayland clipboard even after programs close";
+    };
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+    Service = {
+      ExecStart = "${pkgs.wl-clip-persist}/bin/wl-clip-persist --clipboard regular";
+    };
   };
 }
