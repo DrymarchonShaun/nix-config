3a9eea3e42c00bd3b1802ebb1b13747c0af20162
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-10-24 20:47:09 -0700

    gpg-agent testing

--- home/shaun/common/core/zsh/default.nix
+++ home/shaun/common/core/zsh/default.nix
@@ -50,9 +50,6 @@
     initExtraFirst = ''
        # zmodload zsh/zprof
 
-       export GPG_TTY=$TTY
-       ${config.programs.gpg.package}/bin/gpg-connect-agent updatestartuptty /bye > /dev/null
-
        # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
        # Initialization code that may require console input (password prompts, [y/n]
        # confirmations, etc.) must go above this block; everything else may go below.
@@ -101,6 +98,10 @@
       done
       nix shell "''${args[@]}"
       }
+
+      export GPG_TTY=$TTY
+      ${config.programs.gpg.package}/bin/gpg-connect-agent updatestartuptty /bye > /dev/null
+
 
       # zprof
     '';
