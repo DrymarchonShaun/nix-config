8ab8405e5f8ecaf78ccfa3645b7f824f239a7bd0
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-10-29 14:30:09 -0700

    cnijfilter-mx340: init

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -17,4 +17,5 @@ rec {
   zsh-auto-notify = pkgs.callPackage ./zsh-auto-notify { };
   zsh-term-title = pkgs.callPackage ./zsh-term-title { };
   gamma-launcher = pkgs.callPackage ./gamma-launcher { };
+  cnijfilter-mx340 = pkgs.callPackage ./cnijfilter-mx340 { };
 }
