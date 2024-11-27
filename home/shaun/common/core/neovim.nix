bb4ea88646c2ff7680120a59480e36bde58108c3
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-11-27 12:02:00 -0800

    fix: properly configure nixd's options

--- home/shaun/common/core/neovim.nix
+++ home/shaun/common/core/neovim.nix
@@ -1,4 +1,24 @@
-{ inputs, pkgs, ... }:
 {
-  home.packages = [ inputs.nvix.packages.${pkgs.system}.default ];
+  inputs,
+  pkgs,
+  configVars,
+  osConfig,
+  ...
+}:
+{
+  home.packages = [
+    (inputs.nvix.packages.${pkgs.system}.default.extend {
+      plugins.lsp.servers.nixd.settings =
+        let
+          flake = ''builtins.getFlake "/home/${configVars.username}/.src/nix-config"'';
+        in
+        {
+          nixpkgs.expr = "${flake}.inputs.nixpkgs { }";
+          formatting.command = [ "nixfmt" ];
+          options = {
+            nixOptions.expr = ''(${flake}).nixosConfigurations.${osConfig.networking.hostName}.options'';
+          };
+        };
+    })
+  ];
 }
