1769a5b442078d2a83fe12fedc9ffd0520a93469
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2025-01-23 00:56:17 -0800

    chore: update flake.lock

--- hosts/nixos/natrix/default.nix
+++ hosts/nixos/natrix/default.nix
@@ -13,23 +13,23 @@
   ...
 }:
 let
-  natrixKernel = pkgs.linux_latest.override { };
+  natrixKernel = pkgs.linux_6_11.override { };
   natrixKernelPackages = (pkgs.linuxPackagesFor natrixKernel).extend (
     final: prev: {
-      system76 = (pkgs.linuxPackagesFor natrixKernel).system76.overrideAttrs (attrs: {
-        #   version = "1.0.13-unstable";
-        #   src = attrs.src // {
-        #     rev = "341bcde2d280e384261019baec1496acf5d04d95";
-        #     sha256 = "";
-        #   };
-        patches = [
-          (pkgs.fetchpatch {
-            name = "fix-linux-6_12-build.patch";
-            url = "https://github.com/pop-os/system76-dkms/pull/71.patch";
-            hash = "sha256-skJI1CXwR6rNn3aEPYB7rnvra24W8vtfBmM71d3BD1w=";
-          })
-        ];
-      });
+      # system76 = (pkgs.linuxPackagesFor natrixKernel).system76.overrideAttrs (attrs: {
+      #   #   version = "1.0.13-unstable";
+      #   #   src = attrs.src // {
+      #   #     rev = "341bcde2d280e384261019baec1496acf5d04d95";
+      #   #     sha256 = "";
+      #   #   };
+      #   patches = [
+      #     (pkgs.fetchpatch {
+      #       name = "fix-linux-6_12-build.patch";
+      #       url = "https://github.com/pop-os/system76-dkms/pull/71.patch";
+      #       hash = "sha256-skJI1CXwR6rNn3aEPYB7rnvra24W8vtfBmM71d3BD1w=";
+      #     })
+      #   ];
+      # });
       #    zenergy = final.callPackage ../../pkgs/zenergy { };
     }
   );
