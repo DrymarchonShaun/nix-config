52220db4fb7f31d80ee1ce47e3cecef8434a44be
DrymarchonShaun <DrymarchonShaun@protonmail.com>
2025-01-24 19:59:08 -0800

    fix: actually enable custom block list in ublock origin

--- home/shaun/common/optional/browsers/firefox/ublock-origin.nix
+++ home/shaun/common/optional/browsers/firefox/ublock-origin.nix
@@ -1,63 +1,119 @@
 {
+  # adminSettings = { };
   userSettings = [
     [
       "advancedUserEnabled"
-      true
+      "true"
     ]
     [
       "popupPanelSections"
-      31
+      "31"
+    ]
+    [
+      "cloudStorageEnabled"
+      "true"
+    ]
+    [
+      "prefetchingDisabled"
+      "true"
+    ]
+    [
+      "hyperlinkAuditingDisabled"
+      "true"
+    ]
+    [
+      "cnameUncloakEnabled"
+      "true"
+    ]
+    [
+      "autoUpdate"
+      "true"
+    ]
+    [
+      "suspendUntilListsAreLoaded"
+      "true"
+    ]
+    [
+      "parseAllABPHideFilters"
+      "true"
+    ]
+    # Parse and enforce cosmetic filters
+    [
+      "ignoreGenericCosmeticFilters"
+      "false"
     ]
   ];
   toOverwrite = {
     filterLists = [
       "user-filters"
+
+      # Built-in
       "ublock-filters"
       "ublock-badware"
       "ublock-privacy"
+      "ublock-abuse"
       "ublock-quick-fixes"
       "ublock-unbreak"
+      "ublock-badlists"
+
+      # Ads
       "easylist"
+      "adguard-generic"
+      "adguard-mobile"
+
+      # Privacy
       "easyprivacy"
+      "adguard-spyware"
+      "adguard-spyware-url"
+      "block-lan"
+      "https://divested.dev/blocklists/Fingerprinting.ubl"
+
+      # Malware protection, security
       "urlhaus-1"
+      "curben-phishing"
+
+      # Multipurpose
       "plowe-0"
+      "dpollock-0"
+
+      # Cookie notices (replaces istilldontcareaboutcookies)
       "fanboy-cookiemonster"
       "ublock-cookies-easylist"
+      "adguard-cookies"
+      "ublock-cookies-adguard"
+
+      # Social widgets
+      "fanboy-social"
+      "adguard-social"
+      "fanboy-thirdparty_social"
+
+      # Annoyances
+      "easylist-chat"
+      "easylist-newsletters"
+      "easylist-notifications"
+      "easylist-annoyances"
+      "adguard-mobile-app-banners"
+      "adguard-other-annoyances"
+      "adguard-popup-overlays"
+      "adguard-widgets"
       "ublock-annoyances"
-    ];
-    externalLists = [
+
+      # URL Shortener tools (replaces clearurls)
       "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt"
     ];
-    importedLists = [
-      "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt"
-    ];
-    trustedSiteDerectives = [
-      "about-scheme"
-      "moz-extension-scheme"
-    ];
-  };
-  toAdd = {
-    userFilters = builtins.concatStringsSep "\n" [
+    filters = [
       "##body:style(overflow: auto !important)"
       "##html:style(overflow: auto !important)"
-      "www.youtube.com##+js(nano-stb, resolve(1), *, 0.001)"
-      "www.youtube.com##+js(set, yt.config_.EXPERIMENT_FLAGS.web_enable_ab_rsp_cl, false)"
-      "www.youtube.com##+js(set, yt.config_.EXPERIMENT_FLAGS.ab_pl_man, false)"
-      "||googlevideo.com/videoplayback$xhr,3p,method=get,domain=www.youtube.com"
 
-      "! Dec 7, 2024 https://en.wikipedia.org"
-      "en.wikipedia.org##.cn-fundraising"
+      "www.youtube.com##+js(nano-stb, resolve(1), *, 0.001)"
+      "www.youtube.com##+js(set, yt.config_.experiment_flags.web_enable_ab_rsp_cl, false)"
+      "www.youtube.com##+js(set, yt.config_.experiment_flags.ab_pl_man, false)"
+      "||googlevideo.com/videoplayback$xhr,3p,method=get,domain=www.youtube.com"
+      # hide member only videos
+      "www.youtube.com##:is(ytd-rich-item-renderer,ytd-compact-video-renderer,ytd-video-renderer,ytd-grid-video-renderer):has(.badge-style-type-members-only)"
     ];
-    dynamicFilteringString = builtins.concatStringsSep "\n" [
-      "no-csp-reports: * true"
-      "no-large-media: behind-the-scene false"
-      "behind-the-scene * * noop"
-      "behind-the-scene * 1p-script noop"
-      "behind-the-scene * 3p noop"
-      "behind-the-scene * 3p-frame noop"
-      "behind-the-scene * 3p-script noop"
-      "behind-the-scene * image noop"
-      "behind-the-scene * inline-script noop"
+    trustedSiteDerectives = [
+      "moz-extension-scheme"
     ];
   };
 }
