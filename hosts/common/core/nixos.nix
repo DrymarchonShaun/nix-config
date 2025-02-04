4afb61a124e9373cf25eaadbfc1852e0caf4f16f
DrymarchonShaun <DrymarchonShaun@protonmail.com>
2025-02-03 23:07:19 -0800

    feat: clean tmp on boot

--- hosts/common/core/nixos.nix
+++ hosts/common/core/nixos.nix
@@ -19,7 +19,7 @@
 
   networking.usePredictableInterfaceNames = false;
 
-# This should be handled by config.security.pam.sshAgentAuth.enable
+  # This should be handled by config.security.pam.sshAgentAuth.enable
   security.sudo.extraConfig = ''
     Defaults lecture = never # rollback results in sudo lectures after each reboot, it's somewhat useless anyway
     Defaults pwfeedback # password input feedback - makes typed password visible as asterisks
@@ -27,6 +27,8 @@
     # Keep SSH_AUTH_SOCK so that pam_ssh_agent_auth.so can do its magic.
     Defaults env_keep+=SSH_AUTH_SOCK
   '';
+
+  boot.tmp.cleanOnBoot = true;
 
   # less delay on failed login
   # security.pam.services.login = {
