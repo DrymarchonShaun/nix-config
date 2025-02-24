2875e28619cce95827c3d2bd1fd479f4baecce6a
DrymarchonShaun <DrymarchonShaun@protonmail.com>
2025-02-24 00:02:14 -0800

    feat: switch up wallapaper

--- home/shaun/common/optional/desktops/hyprland/hyprpaper.nix
+++ home/shaun/common/optional/desktops/hyprland/hyprpaper.nix
@@ -7,10 +7,13 @@
       splash = false;
       splash_offset = 2.0;
 
-      preload = [ "${pkgs.wallpapers}/share/backgrounds/nix-black-catppuccin.png" ];
+      # preload = [ "${pkgs.wallpapers}/share/backgrounds/nix-black-catppuccin.png" ];
+      preload = [ "${pkgs.wallpapers}/share/backgrounds/starcitizen-spacec-station.png" ];
 
       wallpaper = [
-        ",${pkgs.wallpapers}/share/backgrounds/nix-black-catppuccin.png"
+        # ",${pkgs.wallpapers}/share/backgrounds/nix-black-catppuccin.png"
+        ",${pkgs.wallpapers}/share/backgrounds/starcitizen-spacec-station.png"
+
       ];
     };
   };
