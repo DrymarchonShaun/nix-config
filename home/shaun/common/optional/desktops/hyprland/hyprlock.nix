019915a368898d8aba9a7501ce420d1c1c949f52
DrymarchonShaun <DrymarchonShaun@protonmail.com>
2025-03-16 21:19:03 -0700

    fix: increase screen lock time

--- home/shaun/common/optional/desktops/hyprland/hyprlock.nix
+++ home/shaun/common/optional/desktops/hyprland/hyprlock.nix
@@ -48,11 +48,11 @@
       };
       listener = [
         {
-          timeout = 300;
+          timeout = 800;
           on-timeout = "loginctl lock-session";
         }
         {
-          timeout = 330;
+          timeout = 1000;
           on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
         }
