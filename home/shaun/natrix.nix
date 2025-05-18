c8b8211ead93d95a8e5b9164b573d09e141a3423
DrymarchonShaun <DrymarchonShaun@protonmail.com>
2025-05-18 12:26:24 -0700

    feat: make xf86touchpadtoggle function in hyprland

--- home/shaun/natrix.nix
+++ home/shaun/natrix.nix
@@ -1,4 +1,36 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  touchpad-toggle = pkgs.writeShellScript "" ''
+    HYPRLAND_DEVICE="elan0412:00-04f3:3240-touchpad"
+    HYPRLAND_VARIABLE="device[$HYPRLAND_DEVICE]:enabled"
+
+    if [ -z "$XDG_RUNTIME_DIR" ]; then
+      export XDG_RUNTIME_DIR=/run/user/$(id -u)
+    fi
+
+    export STATUS_FILE="$XDG_RUNTIME_DIR/touchpad.status"
+
+    enable_touchpad() {
+        printf "true" >"$STATUS_FILE"
+    hyprctl keyword $HYPRLAND_VARIABLE "true" -r
+    }
+
+    disable_touchpad() {
+        printf "false" >"$STATUS_FILE"
+    hyprctl keyword $HYPRLAND_VARIABLE "false" -r
+    }
+
+    if ! [ -f "$STATUS_FILE" ]; then
+      disable_touchpad
+    else
+      if [ $(cat "$STATUS_FILE") = "true" ]; then
+        disable_touchpad
+      elif [ $(cat "$STATUS_FILE") = "false" ]; then
+        enable_touchpad
+      fi
+    fi
+  '';
+in
 {
   imports = [
     #
@@ -27,6 +59,10 @@
   programs.hyprpanel.hasBattery = true;
   wayland.windowManager.hyprland.settings.bindl = [
     ",switch:Lid Switch,exec,pidof hyprlock || hyprlock"
+  ];
+
+  wayland.windowManager.hyprland.settings.bind = [
+    ",XF86TouchpadToggle,exec,${touchpad-toggle}"
   ];
 
   #
