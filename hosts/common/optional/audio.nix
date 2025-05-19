ae260f26de8d2f168d09aec703e9c0c1de4b6b9a
DrymarchonShaun <DrymarchonShaun@protonmail.com>
2025-05-19 14:52:21 -0700

    fix: raise threshold for noise suppression

--- hosts/common/optional/audio.nix
+++ hosts/common/optional/audio.nix
@@ -58,7 +58,7 @@ in
                       plugin = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
                       label = "noise_suppressor_mono";
                       control = {
-                        "VAD Threshold (%)" = 60.0;
+                        "VAD Threshold (%)" = 90.0;
                         "VAD Grace Period (ms)" = 200;
                         "Retroactive VAD Grace (ms)" = 0;
                       };
