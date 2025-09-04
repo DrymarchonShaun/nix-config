ad9754da75130309d44488bdb955b0780ae8e07d
DrymarchonShaun <DrymarchonShaun@protonmail.com>
2025-09-04 15:20:12 -0700

    chore(flake): update nix-index-database module path

--- home/shaun/common/core/default.nix
+++ home/shaun/common/core/default.nix
@@ -30,7 +30,7 @@ in
     ./zoxide.nix
     inputs.catppuccin.homeModules.catppuccin
 
-    inputs.nix-index-database.hmModules.nix-index
+    inputs.nix-index-database.homeModules.nix-index
 
   ];
 
