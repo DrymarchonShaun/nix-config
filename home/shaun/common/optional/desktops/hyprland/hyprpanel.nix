6eaf997146c093802ce11d661c412f7bfb185c35
DrymarchonShaun <DrymarchonShaun@protonmail.com>
2025-09-08 08:59:29 +0200

    fix(hyprpanel): replace network/bluetooth widgets with systray icons, 'properly' apply theme (#59)
    Reviewed-on: https://codeberg.org/DrymarchonShaun/nix-config/pulls/59

--- home/shaun/common/optional/desktops/hyprland/hyprpanel.nix
+++ home/shaun/common/optional/desktops/hyprland/hyprpanel.nix
@@ -12,80 +12,81 @@
       GRIMBLAST_HIDE_CURSOR = 0; # Fix hyprpanel crashing when using grimblast - see https://github.com/Jas-SinghFSU/HyprPanel/issues/888
     };
 
+    services.network-manager-applet.enable = true; # hyprpanel doesn't give much information for networks / bluetooth
+    services.blueman-applet.enable = true;
+
     programs.hyprpanel = {
       enable = true;
       systemd.enable = true;
-      settings = {
-        bar = {
-          launcher.autoDetectIcon = true;
-          network.truncation = false;
-          layouts = {
-            "*" = {
-              left = [
-                "dashboard"
-                "workspaces"
-                "windowtitle"
-              ];
-              middle = [ "media" ];
-              right = [
-                "volume"
-                "clock"
-                "notifications"
-              ];
-            };
-            "0" = {
-              left = [
-                "dashboard"
-                "workspaces"
-                "windowtitle"
-              ];
-              middle = [ "media" ];
-              right = [
-                "volume"
-                "network"
-                "bluetooth"
-                (lib.mkIf config.programs.hyprpanel.hasBattery "battery")
-                "systray"
-                "clock"
-                "notifications"
-              ];
+      settings =
+        (lib.importJSON (config.programs.hyprpanel.package + /share/themes/catppuccin_macchiato.json))
+        // {
+          bar = {
+            launcher.autoDetectIcon = true;
+            network.truncation = false;
+            layouts = {
+              "*" = {
+                left = [
+                  "dashboard"
+                  "workspaces"
+                  "windowtitle"
+                ];
+                middle = [ "media" ];
+                right = [
+                  "volume"
+                  "clock"
+                  "notifications"
+                ];
+              };
+              "0" = {
+                left = [
+                  "dashboard"
+                  "workspaces"
+                  "windowtitle"
+                ];
+                middle = [ "media" ];
+                right = [
+                  "volume"
+                  (lib.mkIf config.programs.hyprpanel.hasBattery "battery")
+                  "systray"
+                  "clock"
+                  "notifications"
+                ];
+              };
             };
           };
-        };
+          theme = {
+            font.name = "Inter";
+            bar.outer_spacing = "0.5em";
+          };
 
-        theme = {
-          name = "catppuccin_macchiato";
-          font.name = "Inter";
-          bar.outer_spacing = "0.5em";
-        };
-
-        notifications.position = "top";
-        menus = {
-          clock.weather.enabled = false;
-          dashboard = {
-            directories.enabled = false;
-            shortcuts = {
-              left = {
-                shortcut1 = {
-                  command = "";
-                  icon = "";
-                  tooltip = "";
-                };
-                shortcut2 = {
-                  command = "";
-                  icon = "";
-                  tooltip = "";
-                };
-                shortcut3 = {
-                  command = "";
-                  icon = "";
-                  tooltip = "";
+          notifications.position = "top";
+          menus = {
+            clock.weather.enabled = false;
+            dashboard = {
+              directories.enabled = false;
+              shortcuts = {
+                left = {
+                  shortcut1 = {
+                    command = "";
+                    icon = "";
+                    tooltip = "";
+                  };
+                  shortcut2 = {
+                    command = "";
+                    icon = "";
+                    tooltip = "";
+                  };
+                  shortcut3 = {
+                    command = "";
+                    icon = "";
+                    tooltip = "";
+                  };
                 };
               };
             };
           };
         };
-      };
     };
   };
 }
