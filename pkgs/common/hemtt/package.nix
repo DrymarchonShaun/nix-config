7897fffed83f421b6ade63dc7a3777e30270a3fa
DrymarchonShaun <DrymarchonShaun@protonmail.com>
2025-09-17 14:24:53 -0700

    feat(pkgs): init hemtt v1.16.4

--- pkgs/common/hemtt/package.nix
+++ pkgs/common/hemtt/package.nix
@@ -1,55 +1,104 @@
 {
+  fetchCrate,
+  fetchFromGitHub,
+  installShellFiles,
+  lib,
+  makeRustPlatform,
+  openssl,
+  pkg-config,
+  replaceVars,
+  rust-bin,
   stdenv,
-  fetchzip,
-  steam-run,
+  writeTextFile,
 }:
-stdenv.mkDerivation rec {
-  pname = "hemtt";
-  version = "1.15.1";
-
-  src = fetchzip {
-    url = "https://github.com/BrettMayson/HEMTT/releases/download/v1.15.1/linux-x64.zip";
-    hash = "sha256-LOAFPAwo0FgfUO3Q3+XAQqF0hSfoRtJjaVbXoIUy2aM=";
+let
+  rustPlatform = makeRustPlatform {
+    cargo = rust-bin.stable.latest.minimal;
+    rustc = rust-bin.stable.latest.minimal;
   };
 
-  buildInputs = [ steam-run ];
+  arma3-wiki = stdenv.mkDerivation rec {
+    name = "arma3-wiki";
+    version = "0.4.1";
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp hemtt $out/bin/
-    chmod +x $out/bin/hemtt
-  '';
+    dontBuild = true;
+
+    src = fetchCrate {
+      pname = "arma3-wiki";
+      inherit version;
+      hash = "sha256-RQuatoHILh/Bj6sARzr+KkFX14kiLPO0GSv3j4QxWtE=";
+    };
+
+    patches = [
+      (replaceVars ./fetch-local-dist.patch {
+        local_dist_repo = fetchFromGitHub {
+          owner = "acemod";
+          repo = "arma3-wiki";
+          rev = "dist";
+          hash = "sha256-lbx3B9WXbt0RDAJMkpssOm2Hm42LzYtYz1Iba5c1TKo=";
+        };
+      })
+    ];
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r . $out
+    '';
+  };
+in
+rustPlatform.buildRustPackage rec {
+  pname = "hemtt";
+  version = "1.16.4";
+
+  src = fetchFromGitHub {
+    owner = "BrettMayson";
+    repo = "HEMTT";
+    rev = "v${version}";
+    hash = "sha256-9M7ByKEPWVQ0uBEr5knmNUtUtVCzQdoic7lcfg/V9uk=";
+  };
+
+  nativeBuildInputs = [
+    installShellFiles
+    pkg-config
+    openssl.dev
+  ];
+
+  HOME = "$TEMPDIR";
+  PKG_CONFIG_PATH = "${openssl.dev}/lib/pkgconfig";
+  doCheck = false;
+
+  cargoPatches = [
+    (writeTextFile {
+      name = "dep-local.patch";
+      text = ''
+        diff --git a/Cargo.toml b/Cargo.toml
+        index d05155f7..3b2fff0b 100644
+        --- a/Cargo.toml
+        +++ b/Cargo.toml
+        @@ -40,7 +40,7 @@ future_incompatible = "warn"
+         nonstandard_style = "warn"
+
+         [workspace.dependencies]
+        -arma3-wiki = "${arma3-wiki.version}"
+        +arma3-wiki = { path = "${arma3-wiki}" }
+         automod =  "1.0.15"
+         byteorder = "1.5.0"
+         chumsky = "0.9.3"
+      '';
+    })
+  ];
+
+  cargoHash = "sha256-wz9QpfhMQnY2ElyOTzc0dVfZiGok8T9MDCLDRuoOj7w=";
+
+  postInstall =
+    ''''
+    + lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+      for bin in hemtt; do
+        installShellCompletion --cmd $bin \
+          --bash <($out/bin/$bin manage completions bash) \
+          --fish <($out/bin/$bin manage completions fish) \
+          --zsh <($out/bin/$bin manage completions zsh)
+        done
+    '';
+
 }
-
-# {
-#   rust-bin,
-#   makeRustPlatform,
-#   fetchFromGitHub,
-#   pkg-config,
-#   openssl,
-# }:
-# let
-#   rustPlatform = makeRustPlatform {
-#     cargo = rust-bin.stable.latest.minimal;
-#     rustc = rust-bin.stable.latest.minimal;
-#   };
-# in
-# rustPlatform.buildRustPackage rec {
-#   pname = "hemtt";
-#   version = "1.15.1";
-#
-#   src = fetchFromGitHub {
-#     owner = "BrettMayson";
-#     repo = "HEMTT";
-#     rev = "v${version}";
-#     hash = "sha256-eOPCQiln6Z/EAcNqO5+7RfvSevV0DzMAD02uTz3VoPY=";
-#   };
-#   nativeBuildInputs = [
-#     pkg-config
-#     openssl.dev
-#   ];
-#
-#   PKG_CONFIG_PATH = "${openssl.dev}/lib/pkgconfig";
-#
-#   cargoHash = "sha256-pA38wekIK58DB427N/rDPCIe66gKT00GesDxXwMAeX0=";
-# }
